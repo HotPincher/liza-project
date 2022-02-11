0329e0ecc3afadbf42cbb631bb2b9211adbd58ff
Sobolefff <89976955+Sobolefff@users.noreply.github.com>
2022-02-11 19:22:26 +0300

    card-list done

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+import {initialCards} from './initial-cards.js'
+
 const profileForms = document.querySelectorAll('.personal-info__form');
 const profileInputs = document.querySelectorAll('.personal-info__input');
 const profileSubmitBtns = document.querySelectorAll('.personal-info__button-submit')
@@ -33,3 +35,30 @@ profileSubmitBtns.forEach(button => button.addEventListener('click', () => {
 	submitForm();
 	disableAdd(button);
 }));
+
+
+const cardList = document.querySelector('.card-list');
+
+// рендер карточек
+const createCard = card => {
+	const cardTemplate = document.querySelector('#cardTemplate');
+	const cardTemplateElement = cardTemplate.content.querySelector('.card-list__base').cloneNode(true);
+	const image = cardTemplateElement.querySelector('.card-list__image');
+	const head = cardTemplateElement.querySelector('.card-list__heading');
+	const tier = cardTemplateElement.querySelector('.card-list__tier');
+	const info = cardTemplateElement.querySelector('.card-list__info');
+	const less = cardTemplateElement.querySelector('#lessons');
+	const time = cardTemplateElement.querySelector('#time');
+	head.textContent = card.name;
+	image.src = card.link;
+	image.alt = card.name;
+	tier.textContent = card.tier;
+	info.textContent = card.info;
+	less.textContent = card.lessons;
+	time.textContent = card.time;
+	return cardTemplateElement;
+	};
+
+// Добавляем карточки
+	const createdCards = initialCards.map(card => createCard(card));
+  	cardList.append(...createdCards);
