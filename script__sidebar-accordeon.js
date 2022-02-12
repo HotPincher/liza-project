44ef0433f74cb5e65cac27bdd1714851e28efd59
Alexander Goncharov <GoncharovNVCHK@yandex.ru>
2022-02-12 16:29:02 +0300

    Add: toggle accordeon.fixed

--- script__sidebar-accordeon.js
+++ script__sidebar-accordeon.js
@@ -5,13 +5,10 @@ const filterBlockHead = document.querySelectorAll(".aside__filter-block-head");
 const detailsAcc = document.querySelectorAll(".aside__filter-items");
 
 // Функция добавления активного класса для filterBlock
-filterBlockHead.forEach(function(item) {
-	item.addEventListener("click", function(evt) {
+filterBlockHead.forEach(function (item) {
+	item.addEventListener("click", function (evt) {
 		evt.stopPropagation();
 		item.parentNode.classList.toggle("aside__filter-block_active");
 	});
 });
-
-//Функция добавления выбранного элемента фильтра в список под sidebar
-
 
