c9f0faf209a99bcb308bccc935308d8d745e9931
Alexander Goncharov <GoncharovNVCHK@yandex.ru>
2022-02-13 17:02:58 +0300

    fix: added the function of adding and removing selected filter elements to the list

--- scripts/open-filter-details.js
+++ scripts/open-filter-details.js
@@ -12,3 +12,88 @@ filterBlockHead.forEach(function (item) {
 	});
 });
 
+const asideFilter = document.querySelector(".aside");
+const checkedItems = document.querySelectorAll(".aside__checkbox-options");
+const checkedList = asideFilter.querySelector(".aside__checked-options-list");
+const checkedGroupItems = document.querySelectorAll('.aside__checkbox-options_group');
+const resetAllBtn = document.querySelector('.aside__reset');
+
+checkedItems.forEach(item => item.addEventListener("click", function () {
+	createCheckedItem(item)
+}));
+
+resetAllBtn.addEventListener('click', function () {
+	checkedGroupItems.forEach(item => item.checked = false);
+
+	while (checkedList.firstChild) {
+		checkedList.removeChild(checkedList.firstChild);
+	}
+})
+
+
+function createCheckedItem(evt) {
+	const checkedItemTemplate = document.querySelector(".checked-item-template").content;
+	const checkedItem = checkedItemTemplate.querySelector(".aside__checked-item").cloneNode(true);
+	const deselectButton = checkedItem.querySelector(".aside__deselect-button");
+	const checkedName = checkedItem.querySelector(".aside__name-checked-option");
+
+	checkedName.textContent = evt.value;
+
+	const itemCollection = checkedList.querySelectorAll(".aside__checked-item");
+
+
+	if (evt.checked == true) {
+		if (evt.value == 'Активный') {
+			checkedGroupItems.forEach(item => item.checked = false);
+			evt.checked = true;
+
+			for (let i = 0; i < [...itemCollection].length; i++) {
+				const elem = [...itemCollection][i].childNodes[1].childNodes[1].textContent;
+
+				if (elem == 'Не активный') {
+					deleteCheckedItem([...itemCollection][i]);
+				}
+			}
+			addCheckedItem(checkedList, checkedItem);
+		}
+
+		if (evt.value == 'Не активный') {
+			checkedGroupItems.forEach(item => item.checked = false);
+			evt.checked = true;
+
+			for (let i = 0; i < [...itemCollection].length; i++) {
+				const elem = [...itemCollection][i].childNodes[1].childNodes[1].textContent;
+
+				if (elem == 'Активный') {
+					deleteCheckedItem([...itemCollection][i]);
+				}
+			}
+			addCheckedItem(checkedList, checkedItem);
+		}
+
+		addCheckedItem(checkedList, checkedItem);
+	}
+
+	if (evt.checked == false) {
+		for (let i = 0; i < [...itemCollection].length; i++) {
+			let elem = [...itemCollection][i].childNodes[1].childNodes[1].textContent
+			if (elem == evt.value) {
+				deleteCheckedItem([...itemCollection][i]);
+			}
+		}
+	}
+
+	deselectButton.addEventListener("click", function () {
+		deleteCheckedItem(checkedItem);
+		evt.checked = false;
+	});
+
+}
+
+function addCheckedItem(parent, child) {
+	parent.prepend(child);
+}
+
+function deleteCheckedItem(element) {
+	element.remove();
+}
