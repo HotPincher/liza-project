56decc97c5725ec6c260a4158a9687898d5bdcac
Gavr72 <sergeygavrilovs72@gmail.com>
2022-05-26 16:37:20 +0500

    fix: выделить чекбоксы в отделный блок checkbox-block

--- scripts/script-test.js
+++ scripts/script-test.js
@@ -1,10 +1,10 @@
 const form = document.querySelector('#form');
-const inputs = form.querySelectorAll('.block-test__input');
-const icons = form.querySelectorAll('.block-test__icon');
-const testText = form.querySelectorAll('.block-test__text');
+const inputs = form.querySelectorAll('.checkbox-block__input');
+const icons = form.querySelectorAll('.checkbox-block__icon');
+const testText = form.querySelectorAll('.checkbox-block__text');
 
-const checkbox = form.querySelectorAll('.block-test__icon_type_checkbox');
-const radio = form.querySelectorAll('.block-test__icon_type_radio');
+const checkbox = form.querySelectorAll('.checkbox-block__icon_type_checkbox');
+const radio = form.querySelectorAll('.checkbox-block__icon_type_radio');
 
 const info = form.querySelector('.block-test__info');
 
@@ -42,34 +42,33 @@ let time = 0;
 form.addEventListener('submit', function (e) {
 	e.preventDefault();
 	for (let i = 0; i < inputs.length; i++) {
-		if (inputs[i].checked && inputs[i].classList.contains('block-test__input_disabled_true')) {
-			icons[i].classList.add('block-test__icon_type_true-check');
-			testText[i].classList.add('block-test__text_theme_true');
+		if (inputs[i].checked && inputs[i].classList.contains('checkbox-block__input_disabled_true')) {
+			icons[i].classList.add('checkbox-block__icon_type_true-check');
+			testText[i].classList.add('checkbox-block__text_theme_true');
 			time++;
 
-		} if (!(inputs[i].checked) && inputs[i].classList.contains('block-test__input_disabled_true')) {
-			icons[i].classList.add('block-test__icon_type_true-uncheck');
+		} if (!(inputs[i].checked) && inputs[i].classList.contains('checkbox-block__input_disabled_true')) {
+			icons[i].classList.add('checkbox-block__icon_type_true-uncheck');
 		}
-		if (inputs[i].checked && inputs[i].classList.contains('block-test__input_disabled_false')) {
-			icons[i].classList.add('block-test__icon_type_false-check');
-			testText[i].classList.add('block-test__text_theme_false');
-		} if (!(inputs[i].checked) && inputs[i].classList.contains('block-test__input_disabled_false')) {
-			icons[i].classList.add('block-test__icon_type_false-uncheck');
-		} if (inputs[i].check) {
+		if (inputs[i].checked && inputs[i].classList.contains('checkbox-block__input_disabled_false')) {
+			icons[i].classList.add('checkbox-block__icon_type_false-check');
+			testText[i].classList.add('checkbox-block__text_theme_false');
+		} if (!(inputs[i].checked) && inputs[i].classList.contains('checkbox-block__input_disabled_false')) {
+			icons[i].classList.add('checkbox-block__icon_type_false-uncheck');
+		} if (inputs[i].checked) {
+			inputs[i].checked = false;
+			inputs[i].disabled = true;
 
 		}
-		inputs[i].checked = false;
-		inputs[i].disabled = true;
+		if (time > 2) {
+			info.classList.add('block-test__info_theme_true');
+		} else {
+			info.classList.add('block-test__info_theme_false');
+		}
 
+		bt.classList.remove('block-test__bt_active');
+		btReset.classList.add('block-test__bt_active');
 	}
-	if (time > 2) {
-		info.classList.add('block-test__info_theme_true');
-	} else {
-		info.classList.add('block-test__info_theme_false');
-	}
-
-	bt.classList.remove('block-test__bt_active');
-	btReset.classList.add('block-test__bt_active');
 });
 
 btReset.addEventListener('click', function () {
