4623c57ed2cd909fa5465e4da7a661bab4f6a1d5
Denis Averchenkov <averchenkov@arrival.com>
2022-05-27 22:23:36 +0400

    sidebar refactoring: - aside deleted - new logic of accordion expanding - new elements for sidebar list

--- scripts/open-filter-details.js
+++ scripts/open-filter-details.js
@@ -1,15 +1,8 @@
-const filterBlockHead = document.querySelectorAll(".aside__filter-block-head");
-const checkedItems = document.querySelectorAll(".aside__checkbox-options");
-const checkedList = document.querySelector(".aside__checked-options-list");
-const checkedGroupItems = document.querySelectorAll(".aside__checkbox-options_group");
-const resetAllBtn = document.querySelector(".aside__reset-button");
+const checkedItems = document.querySelectorAll(".checkbox__options");
+const checkedList = document.querySelector(".sidebar__checked-options-list");
+const checkedGroupItems = document.querySelectorAll(".sidebar__checkbox-options_group");
+const resetAllBtn = document.querySelector(".filter__reset-button");
 
-filterBlockHead.forEach(item => {
-	item.addEventListener("click", function (evt) {
-		evt.stopPropagation();
-		item.parentNode.classList.toggle("aside__filter-block_active");
-	});
-});
 
 checkedItems.forEach(item =>
 	item.addEventListener("click", () => {
@@ -30,7 +23,7 @@ resetAllBtn.addEventListener("click", () => {
 	while (checkedList.firstChild) {
 		checkedList.removeChild(checkedList.firstChild);
 	}
-	resetAllBtn.classList.remove("aside__reset-button_active");
+	resetAllBtn.classList.remove("block-header__button_active");
 });
 
 function createCheckedItem(evt) {
@@ -38,18 +31,18 @@ function createCheckedItem(evt) {
 		".checked-item-template"
 	).content;
 	const checkedItem = checkedItemTemplate
-		.querySelector(".aside__checked-item")
+		.querySelector(".sidebar__checked-item")
 		.cloneNode(true);
-	const deselectButton = checkedItem.querySelector(".aside__deselect-button");
+	const deselectButton = checkedItem.querySelector(".sidebar__deselect-button");
 	const checkedName = checkedItem.querySelector(
-		".aside__name-checked-option"
+		".sidebar__name-checked-option"
 	);
-	const itemCollection = checkedList.querySelectorAll(".aside__checked-item");
+	const itemCollection = checkedList.querySelectorAll(".sidebar__checked-item");
 	checkedName.textContent = evt.value;
 
-	if (evt.checked == true) {
-		resetAllBtn.classList.add("aside__reset-button_active");
-		if (evt.value == "Активный") {
+	if (evt.checked === true) {
+		resetAllBtn.classList.add("block-header__button_active");
+		if (evt.value === "Активный") {
 			checkedGroupItems.forEach(item => (item.checked = false));
 			evt.checked = true;
 
@@ -57,14 +50,14 @@ function createCheckedItem(evt) {
 				const elem = [...itemCollection][i].childNodes[1].childNodes[1]
 					.textContent;
 
-				if (elem == "Не активный") {
+				if (elem === "Не активный") {
 					deleteCheckedItem([...itemCollection][i]);
 				}
 			}
 			addCheckedItem(checkedList, checkedItem);
 		}
 
-		if (evt.value == "Не активный") {
+		if (evt.value === "Не активный") {
 			checkedGroupItems.forEach(item => (item.checked = false));
 			evt.checked = true;
 
@@ -72,7 +65,7 @@ function createCheckedItem(evt) {
 				const elem = [...itemCollection][i].childNodes[1].childNodes[1]
 					.textContent;
 
-				if (elem == "Активный") {
+				if (elem === "Активный") {
 					deleteCheckedItem([...itemCollection][i]);
 				}
 			}
@@ -81,16 +74,16 @@ function createCheckedItem(evt) {
 		addCheckedItem(checkedList, checkedItem);
 	}
 
-	if (evt.checked == false) {
+	if (evt.checked === false) {
 		for (let i = 0; i < [...itemCollection].length; i++) {
 			let elem = [...itemCollection][i].childNodes[1].childNodes[1]
 				.textContent;
-			if (elem == evt.value) {
+			if (elem === evt.value) {
 				deleteCheckedItem([...itemCollection][i]);
 			}
 		}
 		if (checkedList.children.length === 0) {
-			resetAllBtn.classList.remove("aside__reset-button_active");
+			resetAllBtn.classList.remove("block-header__button_active");
 		}
 	}
 
@@ -98,7 +91,7 @@ function createCheckedItem(evt) {
 		deleteCheckedItem(checkedItem);
 		evt.checked = false;
 		if (checkedList.children.length === 0) {
-			resetAllBtn.classList.remove("aside__reset-button_active");
+			resetAllBtn.classList.remove("block-header__button_active");
 		}
 		showCard();
 	});
@@ -120,11 +113,11 @@ function showCard() {
 	const cardBox = document.querySelectorAll(".card-list__base");
 
 	checkedItems.forEach(el => {
-		if (el.checked == true && el.dataset.level === "Уровень") {
+		if (el.checked === true && el.dataset.level === "Уровень") {
 			filterLevel.push(el.value);
 		}
 
-		if (el.checked == true && el.dataset.status === "Статус") {
+		if (el.checked === true && el.dataset.status === "Статус") {
 			filterStatus.push(el.dataset.btn);
 		}
 	});
@@ -132,16 +125,16 @@ function showCard() {
 	cardBox.forEach(e => {
 		e.classList.remove("card-list__base_shattered");
 
-		if (filterLevel.length == 0 && filterStatus.length == 0) {
+		if (filterLevel.length === 0 && filterStatus.length === 0) {
 			e.classList.remove("card-list__base_shattered");
 		} else if (
-			filterLevel.includes(e.dataset.card) == false &&
-			filterLevel.length != 0
+			filterLevel.includes(e.dataset.card) === false &&
+			filterLevel.length !== 0
 		) {
 			e.classList.add("card-list__base_shattered");
 		} else if (
-			filterStatus.includes(e.dataset.status) == false &&
-			filterStatus.length != 0
+			filterStatus.includes(e.dataset.status) === false &&
+			filterStatus.length !== 0
 		) {
 			e.classList.add("card-list__base_shattered");
 		}
