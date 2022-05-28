9ebddddf49266ece49656168ce6f434ee89036e1
Gavr72 <sergeygavrilovs72@gmail.com>
2022-05-28 17:05:56 +0500

    fead: добавить блоки 'test-result' в тест

--- scripts/script-test.js
+++ scripts/script-test.js
@@ -2,6 +2,7 @@ const form = document.querySelector('#form');
 const inputs = form.querySelectorAll('.checkbox-block__input');
 const icons = form.querySelectorAll('.checkbox-block__icon');
 const testText = form.querySelectorAll('.checkbox-block__text');
+const items = form.querySelectorAll('.block-test__form-items');
 
 const checkbox = form.querySelectorAll('.checkbox-block__icon_type_checkbox');
 const radio = form.querySelectorAll('.checkbox-block__icon_type_radio');
@@ -10,7 +11,6 @@ const info = form.querySelector('.block-test__info');
 
 const bt = form.querySelector('.block-test__bt');
 const btReset = form.querySelector('.block-test__bt_type_reset');
-
 
 
 form.addEventListener('change', function (e) {
@@ -60,15 +60,35 @@ form.addEventListener('submit', function (e) {
 			inputs[i].disabled = true;
 
 		}
-		if (time > 2) {
-			info.classList.add('block-test__info_theme_true');
-		} else {
-			info.classList.add('block-test__info_theme_false');
-		}
 
-		bt.classList.remove('block-test__bt_active');
-		btReset.classList.add('block-test__bt_active');
 	}
+	console.log(time);
+	if (time > 2) {
+		bt.insertAdjacentHTML('beforebegin', `
+		<section class="test-result test-result_color_green">
+			<div aria-label="Результат" class="test-result__result">83%</div>
+				<div class="test-result__container">
+					<p class="test-result__paragraph-indent">Oтличный результат!</p>
+					<p class="test-result__paragraph-indent">Нажите «Далее» чтобы продолжить.</p> Если считаете, что сможете
+					лучше, нажмите «Пересдать».
+					В случае если результат будет хуже, засчитается наивысший результат.
+				</div>
+		</section>
+		`);
+	} else {
+		bt.insertAdjacentHTML('beforebegin', `
+		<section class="test-result test-result_color_red">
+			<div aria-label="Результат" class="test-result__result">33%</div>
+				<div class="test-result__container">
+					<p class="test-result__paragraph-indent">К сожалению, вы не набрали проходной результат.</p> Нажмите
+					«Пересдать», чтобы попробовать снова.
+			</div>
+		</section>
+		`);
+	}
+
+	bt.classList.remove('block-test__bt_active');
+	btReset.classList.add('block-test__bt_active');
 });
 
 btReset.addEventListener('click', function () {
