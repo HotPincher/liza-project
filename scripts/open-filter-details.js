249ae41e51c2cec87d7f1855214bf3ab1c117fd0
Denis Averchenkov <averchenkov@arrival.com>
2022-05-31 13:43:58 +0400

    bem fixes buttons fixes

--- scripts/open-filter-details.js
+++ scripts/open-filter-details.js
@@ -1,142 +1,142 @@
-const checkedItems = document.querySelectorAll(".checkbox__options");
-const checkedList = document.querySelector(".sidebar__checked-options-list");
-const checkedGroupItems = document.querySelectorAll(".sidebar__checkbox-options_group");
-const resetAllBtn = document.querySelector(".filter__reset-button");
+const checkedItems = document.querySelectorAll('.checkbox__options');
+const checkedList = document.querySelector('.sidebar__checked-options-list');
+const checkedGroupItems = document.querySelectorAll('.checkbox__options_group');
+const resetAllBtn = document.querySelector('.button_type_filter-reset');
 
 
 checkedItems.forEach(item =>
-	item.addEventListener("click", () => {
-		createCheckedItem(item);
-	})
+    item.addEventListener('click', () => {
+        createCheckedItem(item);
+    })
 );
 
-resetAllBtn.addEventListener("click", () => {
-	checkedGroupItems.forEach(item => (item.checked = false));
-	showCard();
+resetAllBtn.addEventListener('click', () => {
+    checkedGroupItems.forEach(item => (item.checked = false));
+    showCard();
 
-	const cardBox = document.querySelectorAll(".card-list__base");
+    const cardBox = document.querySelectorAll('.card-list__base');
 
-	cardBox.forEach(e => {
-		e.classList.remove("card-list__base_shattered");
-	});
+    cardBox.forEach(e => {
+        e.classList.remove('card-list__base_shattered');
+    });
 
-	while (checkedList.firstChild) {
-		checkedList.removeChild(checkedList.firstChild);
-	}
-	resetAllBtn.classList.remove("block-header__button_active");
+    while (checkedList.firstChild) {
+        checkedList.removeChild(checkedList.firstChild);
+    }
+    resetAllBtn.classList.remove('block-header__button_active');
 });
 
 function createCheckedItem(evt) {
-	const checkedItemTemplate = document.querySelector(
-		".checked-item-template"
-	).content;
-	const checkedItem = checkedItemTemplate
-		.querySelector(".sidebar__checked-item")
-		.cloneNode(true);
-	const deselectButton = checkedItem.querySelector(".sidebar__deselect-button");
-	const checkedName = checkedItem.querySelector(
-		".sidebar__name-checked-option"
-	);
-	const itemCollection = checkedList.querySelectorAll(".sidebar__checked-item");
-	checkedName.textContent = evt.value;
+    const checkedItemTemplate = document.querySelector(
+        '.checked-item-template'
+    ).content;
+    const checkedItem = checkedItemTemplate
+        .querySelector('.sidebar__checked-item')
+        .cloneNode(true);
+    const deselectButton = checkedItem.querySelector('.sidebar__deselect-button');
+    const checkedName = checkedItem.querySelector(
+        '.sidebar__name-checked-option'
+    );
+    const itemCollection = checkedList.querySelectorAll('.sidebar__checked-item');
+    checkedName.textContent = evt.value;
 
-	if (evt.checked === true) {
-		resetAllBtn.classList.add("block-header__button_active");
-		if (evt.value === "Активный") {
-			checkedGroupItems.forEach(item => (item.checked = false));
-			evt.checked = true;
+    if (evt.checked === true) {
+        resetAllBtn.classList.add('block-header__button_active');
+        if (evt.value === 'Активный') {
+            checkedGroupItems.forEach(item => (item.checked = false));
+            evt.checked = true;
 
-			for (let i = 0; i < [...itemCollection].length; i++) {
-				const elem = [...itemCollection][i].childNodes[1].childNodes[1]
-					.textContent;
+            for (let i = 0; i < [...itemCollection].length; i++) {
+                const elem = [...itemCollection][i].childNodes[1].childNodes[1]
+                    .textContent;
 
-				if (elem === "Не активный") {
-					deleteCheckedItem([...itemCollection][i]);
-				}
-			}
-			addCheckedItem(checkedList, checkedItem);
-		}
+                if (elem === 'Не активный') {
+                    deleteCheckedItem([...itemCollection][i]);
+                }
+            }
+            addCheckedItem(checkedList, checkedItem);
+        }
 
-		if (evt.value === "Не активный") {
-			checkedGroupItems.forEach(item => (item.checked = false));
-			evt.checked = true;
+        if (evt.value === 'Не активный') {
+            checkedGroupItems.forEach(item => (item.checked = false));
+            evt.checked = true;
 
-			for (let i = 0; i < [...itemCollection].length; i++) {
-				const elem = [...itemCollection][i].childNodes[1].childNodes[1]
-					.textContent;
+            for (let i = 0; i < [...itemCollection].length; i++) {
+                const elem = [...itemCollection][i].childNodes[1].childNodes[1]
+                    .textContent;
 
-				if (elem === "Активный") {
-					deleteCheckedItem([...itemCollection][i]);
-				}
-			}
-			addCheckedItem(checkedList, checkedItem);
-		}
-		addCheckedItem(checkedList, checkedItem);
-	}
+                if (elem === 'Активный') {
+                    deleteCheckedItem([...itemCollection][i]);
+                }
+            }
+            addCheckedItem(checkedList, checkedItem);
+        }
+        addCheckedItem(checkedList, checkedItem);
+    }
 
-	if (evt.checked === false) {
-		for (let i = 0; i < [...itemCollection].length; i++) {
-			let elem = [...itemCollection][i].childNodes[1].childNodes[1]
-				.textContent;
-			if (elem === evt.value) {
-				deleteCheckedItem([...itemCollection][i]);
-			}
-		}
-		if (checkedList.children.length === 0) {
-			resetAllBtn.classList.remove("block-header__button_active");
-		}
-	}
+    if (evt.checked === false) {
+        for (let i = 0; i < [...itemCollection].length; i++) {
+            let elem = [...itemCollection][i].childNodes[1].childNodes[1]
+                .textContent;
+            if (elem === evt.value) {
+                deleteCheckedItem([...itemCollection][i]);
+            }
+        }
+        if (checkedList.children.length === 0) {
+            resetAllBtn.classList.remove('block-header__button_active');
+        }
+    }
 
-	deselectButton.addEventListener("click", () => {
-		deleteCheckedItem(checkedItem);
-		evt.checked = false;
-		if (checkedList.children.length === 0) {
-			resetAllBtn.classList.remove("block-header__button_active");
-		}
-		showCard();
-	});
-	showCard();
+    deselectButton.addEventListener('click', () => {
+        deleteCheckedItem(checkedItem);
+        evt.checked = false;
+        if (checkedList.children.length === 0) {
+            resetAllBtn.classList.remove('block-header__button_active');
+        }
+        showCard();
+    });
+    showCard();
 }
 
 function addCheckedItem(parent, child) {
-	parent.prepend(child);
+    parent.prepend(child);
 }
 
 function deleteCheckedItem(element) {
-	element.remove();
+    element.remove();
 }
 
 function showCard() {
-	let filterLevel = [];
-	let filterStatus = [];
+    let filterLevel = [];
+    let filterStatus = [];
 
-	const cardBox = document.querySelectorAll(".card-list__base");
+    const cardBox = document.querySelectorAll('.card-list__base');
 
-	checkedItems.forEach(el => {
-		if (el.checked === true && el.dataset.level === "Уровень") {
-			filterLevel.push(el.value);
-		}
+    checkedItems.forEach(el => {
+        if (el.checked === true && el.dataset.level === 'Уровень') {
+            filterLevel.push(el.value);
+        }
 
-		if (el.checked === true && el.dataset.status === "Статус") {
-			filterStatus.push(el.dataset.btn);
-		}
-	});
+        if (el.checked === true && el.dataset.status === 'Статус') {
+            filterStatus.push(el.dataset.btn);
+        }
+    });
 
-	cardBox.forEach(e => {
-		e.classList.remove("card-list__base_shattered");
+    cardBox.forEach(e => {
+        e.classList.remove('card-list__base_shattered');
 
-		if (filterLevel.length === 0 && filterStatus.length === 0) {
-			e.classList.remove("card-list__base_shattered");
-		} else if (
-			filterLevel.includes(e.dataset.card) === false &&
+        if (filterLevel.length === 0 && filterStatus.length === 0) {
+            e.classList.remove('card-list__base_shattered');
+        } else if (
+            filterLevel.includes(e.dataset.card) === false &&
 			filterLevel.length !== 0
-		) {
-			e.classList.add("card-list__base_shattered");
-		} else if (
-			filterStatus.includes(e.dataset.status) === false &&
+        ) {
+            e.classList.add('card-list__base_shattered');
+        } else if (
+            filterStatus.includes(e.dataset.status) === false &&
 			filterStatus.length !== 0
-		) {
-			e.classList.add("card-list__base_shattered");
-		}
-	});
+        ) {
+            e.classList.add('card-list__base_shattered');
+        }
+    });
 }
